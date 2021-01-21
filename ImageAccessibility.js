c9945acf9adec22b24941c8aab6de19bf1bbd5a8
yaron <yaron57@gmail.com>
2021-01-21 14:02:28 -0500

    Initial version

---
+++ ImageAccessibility.js
@@ -0,0 +1,22 @@
+( function ( $, mw ) {
+
+	$('img[data-long-desc-url]').each( function() {
+		var longDescURL = $(this).attr('data-long-desc-url');
+		var longDescLink = '<p><a href="' + longDescURL + '">' +
+			'View description of this image' + '</a></p>';
+		$(longDescLink).insertAfter($(this).parent());
+	});
+	$('img[data-create-long-desc-url]').each( function() {
+		var longDescURL = $(this).attr('data-create-long-desc-url');
+		var longDescLink = '<p><a href="' + longDescURL + '" class="new">' +
+			'Create long description of this image' + '</a></p>';
+		$(longDescLink).insertAfter($(this).parent());
+	});
+	$('img[data-view-long-desc-url]').each( function() {
+		var longDescURL = $(this).attr('data-view-long-desc-url');
+		var longDescLink = '<p><a href="' + longDescURL + '">' +
+			'View long description of this image' + '</a></p>';
+		$(longDescLink).insertAfter($(this).parent());
+	});
+
+}( jQuery, mediaWiki ) );
---
+++ ImageAccessibility.php
@@ -0,0 +1,37 @@
+<?php
+
+class ImageAccessibility {
+
+        public static function addRLModules( OutputPage $out, Skin $skin ) {
+                $out->addModules( 'ext.imageaccessibility' );
+                return true;
+        }
+
+	public static function addLongDescURL( ThumbnailImage $thumbnail, array &$attribs, array &$linkAttribs ) {
+		global $wgTitle, $wgImageAccessibilitySuffix;
+
+		$imageName = $thumbnail->getFile()->getTitle()->getFullText();
+		$longDescPageName = $imageName . $wgImageAccessibilitySuffix;
+		$longDescPage = Title::newFromText( $longDescPageName );
+
+		if ( $wgTitle->getNamespace() == NS_FILE ) {
+			if ( !$longDescPage->exists() ) {
+				$longDescURL = $longDescPage->getInternalURL( [ 'action' => 'edit', 'redlink' => '1' ]);
+				$attribs['data-create-long-desc-url'] = $longDescURL;
+			} else {
+				$longDescURL = $longDescPage->getInternalURL();
+				$attribs['data-view-long-desc-url'] = $longDescURL;
+			}
+			return true;
+		}
+
+		if ( !$longDescPage->exists() ) {
+			return true;
+		}
+		$longDescURL = $longDescPage->getInternalURL( [ 'action' => 'render' ]);
+		$attribs['data-long-desc-url'] = $longDescURL;
+
+		return true;
+	}
+
+}
