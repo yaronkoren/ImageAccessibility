3e02dfa9ce71f2a08e7186ea8188afed5481da12
yaron <yaron57@gmail.com>
2021-05-06 11:04:44 -0400

    Make JS texts i18n message (and slightly change one)

--- ImageAccessibility.js
+++ ImageAccessibility.js
@@ -3,19 +3,19 @@
 	$('img[data-long-desc-url]').each( function() {
 		var longDescURL = $(this).attr('data-long-desc-url');
 		var longDescLink = '<p class="imageLongDescription"><a href="' + longDescURL + '">' +
-			'View description of this image' + '</a></p>';
+			mw.msg( 'imageaccessibility-viewdesc' ) + '</a></p>';
 		$(longDescLink).insertAfter($(this).parent());
 	});
 	$('img[data-create-long-desc-url]').each( function() {
 		var longDescURL = $(this).attr('data-create-long-desc-url');
 		var longDescLink = '<p><a href="' + longDescURL + '" class="new">' +
-			'Create long description of this image' + '</a></p>';
+			mw.msg( 'imageaccessibility-createdesc' ) + '</a></p>';
 		$(longDescLink).insertAfter($(this).parent());
 	});
 	$('img[data-view-long-desc-url]').each( function() {
 		var longDescURL = $(this).attr('data-view-long-desc-url');
 		var longDescLink = '<p><a href="' + longDescURL + '">' +
-			'View long description of this image' + '</a></p>';
+			mw.msg( 'imageaccessibility-viewdescinternal' ) + '</a></p>';
 		$(longDescLink).insertAfter($(this).parent());
 	});
 
